047de2a45798b4a929f820e7e8487183579e2d44
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-18 21:29:57 -0600

    Hover effect on grid squares

--- js/etch.js
+++ js/etch.js
@@ -3,10 +3,17 @@ const grid = document.querySelector('.container');
 function createGrid(){
     for(i = 0; i < 16 ; i++){
         for(j = 0; j < 16 ; j++){
-            const rows = document.createElement('div');
-            grid.appendChild(rows);
+            const squares = document.createElement('div');
+            squares.classList.add("gridSquare");
+            grid.appendChild(squares);
         }
     }
 }
+
+const color = document.querySelector('div');
+color.addEventListener("mouseover", function(event){
+    event.target.classList.replace("gridSquare","changeColor");
+});
+
 
 createGrid();
