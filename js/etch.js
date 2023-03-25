b8b8d8552b3be3629686038f4698571776db0390
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-24 19:18:32 -0600

    Upgrade function and minor changes

--- js/etch.js
+++ js/etch.js
@@ -1,8 +1,8 @@
 const grid = document.querySelector('.container');
 
-function createGrid(){
-    for(i = 0; i < 16 ; i++){
-        for(j = 0; j < 16 ; j++){
+function createGrid() {
+    for (i = 0; i < 16; i++) {
+        for (j = 0; j < 16; j++) {
             const squares = document.createElement('div');
             squares.classList.add("gridSquare");
             grid.appendChild(squares);
@@ -11,9 +11,24 @@ function createGrid(){
 }
 
 const color = document.querySelector('div');
-color.addEventListener("mouseover", function(event){
-    event.target.classList.replace("gridSquare","changeColor");
+color.addEventListener("mouseover", function (event) {
+    event.target.classList.replace("gridSquare", "changeColor");
 });
+
+function updateGrid() {
+    grid.innerHTML="";
+    const update = document.getElementById('size').value;
+    grid.style.setProperty("grid-template-columns",`repeat(${update}, 2fr)`);
+    grid.style.setProperty("grid-template-rows",`repeat(${update}, 2fr)`);
+    for (i = 0; i < update; i++) {
+        for (j = 0; j < update; j++) {
+            const squares = document.createElement('div');
+            squares.classList.add("gridSquare");
+            grid.appendChild(squares);
+        }
+    }
+}
+
 
 
 createGrid();
